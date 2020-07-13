db2d232e3524b5a1ad184bd100f847ed7624aced
Sebastián Sánchez <ssp013sanchez@gmail.com>
2020-07-12 20:25:47 -0400

    d
    d

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const fila = document.querySelector('.contenedor-carousel') ; //Contenedor carousel
+const peliculas = document.querySelector('.pelicula');
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+//
